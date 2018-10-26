32ca05f5964173b61723bcdc4399d876659d6dfb
Tony Mason <wamason@gmail.com>
2018-10-26 13:46:18 -0700

    quantize the allocation unit to be 64 bytes.

--- benchmark/src/bench_alloc_free_alloc.cpp
+++ benchmark/src/bench_alloc_free_alloc.cpp
@@ -4,8 +4,9 @@
 #include <sstream>
 
 std::vector<uint64_t> workerTimes;
-uint64_t allocation_size_min = 64;
-uint64_t allocation_size_max = 64;
+static const uint64_t allocation_unit_size = 64; // cache line
+uint64_t allocation_size_min = 1;
+uint64_t allocation_size_max = 1;
 
 void worker(int id) {
     volatile char* pointerlist[100000];
@@ -16,7 +17,8 @@ void worker(int id) {
 
     timer.start();
     for (int i=0; i<100000; ++i) {
-        pointerlist[i] = (volatile char*) nvb::reserve(randomSize());
+        uint64_t size = allocation_unit_size * randomSize();
+        pointerlist[i] = (volatile char*) nvb::reserve(size);
         memset((void*)pointerlist[i], 5, 64);
         nvb::activate((void*) pointerlist[i]);
     }
@@ -24,7 +26,8 @@ void worker(int id) {
         nvb::free((void*) pointerlist[i]);
     }
     for (int i=0; i<100000; ++i) {
-        pointerlist[i] = (volatile char*) nvb::reserve(randomSize());
+        uint64_t size = allocation_unit_size * randomSize();
+        pointerlist[i] = (volatile char*) nvb::reserve(size);
         memset((void*)pointerlist[i], 5, 64);
         nvb::activate((void*) pointerlist[i]);
     }
@@ -40,15 +43,25 @@ int main(int argc, char **argv) {
     }
     size_t n_threads = atoi(argv[1]);
     allocation_size_min = atoi(argv[2]);
-    if (allocation_size_min < 64) {
+    if (allocation_size_min < allocation_unit_size) {
         std::cout << "WARNING: specified min allocation size was less than minimum, using 64 bytes instead" << std::endl;
-        allocation_size_min = 64;
+        allocation_size_min = 1;
+    }
+    else {
+        allocation_size_min /= allocation_unit_size;
     }
     if (argc == 4) {
         allocation_size_max = atoi(argv[3]);
         if (allocation_size_max < allocation_size_min) {
             std::cout << "WARNING: max allocation size was less than min, using min instead" << std::endl;
             allocation_size_max = allocation_size_min;
+        }
+        uint64_t max = allocation_size_max /= allocation_unit_size;
+        if (max * allocation_unit_size != allocation_size_max) {
+            allocation_size_max = max + 1; // round up
+        }
+        else {
+            allocation_size_max = max;
         }
     } else {
         allocation_size_max = allocation_size_min;
